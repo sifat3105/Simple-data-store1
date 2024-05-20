6891df6c6cf0ea14372c82af5d86ceb0fdf0f2ab
MD SIFAT ALI <sifatali520@gmail.com>
2024-05-20 23:18:00 +0600

    Update views.py

--- home/views.py
+++ home/views.py
@@ -6,21 +6,22 @@ import os
 # Create your views here.
 def home_func(request):
     if request.method == 'POST':
-        massege = "enter name and age"
+        message = 'Enter Name and Age'
+        print(message)
         name = request.POST.get('name')
         age = request.POST.get('age')
         image = request.FILES.get('image')
         if Human.objects.filter(name=name).exists():
-            massege = 'data already exist'
+            message = 'data already exist'
         else:
             Human.objects.create(name=name, age=age, image=image)
             # data.save()
-            massege = 'data added'
-        search_query = request.GET.get('search')
-        
+            message = 'data added'
+          
     search_query = request.GET.get('search')
     if search_query:
-        all_data = Human.objects.filter(name__icontains = search_query)        
+        all_data = Human.objects.filter(name__icontains = search_query)
+        message = 'Search result...'       
     else:
         all_data = Human.objects.all()
     return render(request,'home.html', locals())
@@ -39,14 +40,14 @@ def update_data(request,id):
         age = request.POST.get('age')
         image = request.FILES.get('image')
         if Human.objects.filter(Q(name=name) & Q(age=age)).exists():
-            massege = 'data already exist'
+            message = 'data already exist'
         else:
 
             data.name = name
             data.age = age
             data.image = image
             data.save()
-            massege = 'data added'
+            message = 'data added'
             return redirect('/')
     
     return render(request,'update.html', locals())
